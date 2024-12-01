43a47e9dc375b1ecc22d2ec3de19100b89506878
ranamiah221 <ranarasul21@gmail.com>
2024-12-01 16:11:25 +0600

    added job details pages and and dynamic job card

--- client/src/components/Job/Job.jsx
+++ client/src/components/Job/Job.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Job = ({job}) => {
-  console.log(job)
+  const{_id, deadline, category, job_title, description, min_price, max_price, }=(job)
     return (
-        <div className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
+        <Link to={`/jobs/${_id}`} className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
       <div className='flex items-center justify-between'>
         <span className='text-xs font-light text-gray-800 '>
-          Deadline: 20/12/2024
+          {new Date().toLocaleDateString({deadline})}
         </span>
         <span className='px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full '>
-          Web Development
+         {category}
         </span>
       </div>
 
       <div>
         <h1 className='mt-2 text-lg font-semibold text-gray-800 '>
-          Build Dynamic Website Using React
+          {job_title}
         </h1>
 
         <p className='mt-2 text-sm text-gray-600 '>
-          Lorem ipsum dolor sit adipisicing elit...
+          {description.substring(0,50)+ "....."}
         </p>
         <p className='mt-2 text-sm font-bold text-gray-600 '>
-          Range: $100 - $150
+          Range: ${min_price} - ${max_price}
         </p>
       </div>
-    </div>
+    </Link>
   
     );
 };
--- client/src/router/Routes.jsx
+++ client/src/router/Routes.jsx
@@ -9,6 +9,7 @@ import Login from "../pages/Login/Login";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Registration from "../pages/Registration/Registration";
 import AddJobs from "../pages/AddJobs/AddJobs";
+import JobDetails from "../pages/JobDetails/JobDetails";
 
 const router = createBrowserRouter([
     {
@@ -32,6 +33,12 @@ const router = createBrowserRouter([
                 path:'/addJobs',
                 element:<AddJobs></AddJobs>
             },
+            {
+                path:'/jobs/:id',
+                element:<JobDetails></JobDetails>,
+                loader:({params})=>fetch(`http://localhost:9000/jobs/${params.id}`)
+            },
+
         ]
     }
 
